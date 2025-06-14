ce08e115b727cac8072514f5e0af5a459c47ae07
Luis Orta <lorta4@wgu.edu>
2025-06-14 10:13:04 -0700

    Disabling caching

--- CodersUnite/layout.ps1
+++ CodersUnite/layout.ps1
@@ -37,6 +37,9 @@ pre, code {
                 }
             ) -join [Environment]::NewLine            
         )
+    <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate">
+    <meta http-equiv="Pragma" content="no-cache">
+    <meta http-equiv="Expires" content="0">
     </head>
 <body>
 
